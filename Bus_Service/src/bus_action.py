c2cf11e57cb4fb27cd579688f3bdf41102407525
VIBHAV19 <vpissurlenkar23@gmail.com>
2023-02-03 12:53:42 +0530

    Added complete bus action code

---
+++ Bus_Service/src/bus_action.py
@@ -0,0 +1,40 @@
+from bus_repository import BusRepository
+
+class BusActions:
+  def __init__(self) -> None:
+    self.bus_repo = BusRepository()
+
+  def get_all_bus(self):
+    try:
+      bus = self.bus_repo.get_all_bus()
+      res = []
+      for bus in bus:
+        res.append({
+          'id': bus[0],
+          'Departure_time': bus[1],
+          'Arrival_time': bus[2],
+          'Beginning': bus[3],
+          'Destination' : bus[4],
+          'Bus_number' : bus[5],
+
+        })
+      return res
+    except Exception as e:
+      print(e)
+      return {}
+
+  def add_bus(self,depart,arrive,fromdes,todes,number):
+    try:
+      bus = self.bus_repo.add_bus(depart,arrive,fromdes,todes,number)
+      return bus
+    except Exception as e:
+      print(e)
+      return {}
+
+  def delete_bus(self,id):
+    try:
+      bus = self.bus_repo.delete_bus(id)
+      return bus
+    except Exception as e:
+      print(e)
+      return {}
